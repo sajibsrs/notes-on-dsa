ef9af0613f544efda0610c0e1e722f3bfabe4f93
Sajidur Rahman <sajibsrs@gmail.com>
2021-09-11 15:33:42 +0600

    initial commit

---
+++ array.py
@@ -0,0 +1,20 @@
+# Array: The foundation of data structure
+array = ["apples", "bananas", "cucumbers", "dates", "elderberries"]
+
+# Read
+# -- 1 step
+
+# Search
+# -- Linear search
+# ---- N steps
+
+# Insert
+# -- At the end 1 step
+# -- N + 1 steps
+
+# Delete
+# -- N steps
+
+# Set
+# -- Insert
+# ---- Search ->
